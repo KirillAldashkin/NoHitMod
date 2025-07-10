6583e0bc11c7aed481f97927ecd264723ca45fbc
KirillAldashkin <a89086520092k@yandex.ru>
2025-07-10 21:04:05 +0800

    Check all (?) blocks, remove crack sprite

--- src/nukleer/mindustry/nohit/NoHitMod.kt
+++ src/nukleer/mindustry/nohit/NoHitMod.kt
@@ -1,7 +1,10 @@
 package nukleer.mindustry.nohit
 
+import arc.Core
+import mindustry.Vars
 import mindustry.game.*
 import mindustry.gen.*
+import mindustry.graphics.BlockRenderer
 
 class NoHitMod : mindustry.mod.Mod() {
     val forceHealth = 1.4E-45f
@@ -14,11 +17,23 @@ class NoHitMod : mindustry.mod.Mod() {
     }
 
     init {
+        // remove cracks texture since all blocks
+        // are "severely damaged" with this mod
+        arc.Events.on(EventType.ClientLoadEvent::class.java) {
+            val emptySprite = Core.atlas.find("nohit-challenge-mod-empty")
+            if (emptySprite.width != 1) throw Exception("'empty' not found")
+            for (size in 1..BlockRenderer.maxCrackSize) {
+                for (i in 0 until BlockRenderer.crackRegions) {
+                    Vars.renderer.blocks.cracks[size - 1][i] = emptySprite
+                }
+            }
+        }
+
         // this might not be the most performant way, but it deals with
         // any unexpected interaction e.g. some obscure healing from mods
         arc.Events.run(EventType.Trigger.update) {
             Groups.unit.forEach { verifyEntity(it) }
-            Groups.build.forEach { verifyEntity(it) }
+            Vars.world.tiles.forEach { verifyEntity(it.build) }
         }
     }
 }
